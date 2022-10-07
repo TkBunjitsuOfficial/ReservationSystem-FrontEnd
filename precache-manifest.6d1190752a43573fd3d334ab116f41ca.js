8ff8b037f6458e7e0f7998cf95efb92a8b5b2c9d
kaage <kateageage@gmail.com>
2022-10-08 01:20:58 +0900

    Updates

--- precache-manifest.6d1190752a43573fd3d334ab116f41ca.js
+++ precache-manifest.6d1190752a43573fd3d334ab116f41ca.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "59435a661f02d4cf5fa9",
+    "revision": "1caebed9d8781ad81876",
     "url": "/ReservationSystem/static/css/main.3ad5492b.chunk.css"
   },
   {
-    "revision": "59435a661f02d4cf5fa9",
-    "url": "/ReservationSystem/static/js/main.e39e2506.chunk.js"
+    "revision": "1caebed9d8781ad81876",
+    "url": "/ReservationSystem/static/js/main.e4d95543.chunk.js"
   },
   {
     "revision": "670e4e2f0dd65d5c48e1",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/ReservationSystem/static/js/3.e5ec95a6.chunk.js"
   },
   {
-    "revision": "7e69cec4c2753a902008cc32194ece40",
+    "revision": "b04ff753317d91da28e197aeeade5bf8",
     "url": "/ReservationSystem/index.html"
   }
 ];
