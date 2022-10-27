ddaff06b5d95428ac31c99d696c612e42c6c5cbb
kaage <kateageage@gmail.com>
2022-10-28 08:18:27 +0900

    Updates

--- precache-manifest.68753d8971d6280c50022c02ac116ec4.js
+++ precache-manifest.68753d8971d6280c50022c02ac116ec4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "bbde9c31aa1e4ef95339",
+    "revision": "6c35ac76a4cf7de695bc",
     "url": "/ReservationSystem/static/css/main.04969c6f.chunk.css"
   },
   {
-    "revision": "bbde9c31aa1e4ef95339",
-    "url": "/ReservationSystem/static/js/main.f4a72567.chunk.js"
+    "revision": "6c35ac76a4cf7de695bc",
+    "url": "/ReservationSystem/static/js/main.b4e797c9.chunk.js"
   },
   {
     "revision": "7cf38a1920caecaffcb8",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/ReservationSystem/static/js/3.e2bbc8bc.chunk.js"
   },
   {
-    "revision": "a446d3c76df3fef8620b5696c8505ec8",
+    "revision": "0f2c41ef599bd6490294c02d623b9892",
     "url": "/ReservationSystem/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReservationSystem/precache-manifest.3ae6f364f2bd2ad5df89dde934a1a55f.js"
+  "/ReservationSystem/precache-manifest.68753d8971d6280c50022c02ac116ec4.js"
 );
 
 workbox.clientsClaim();
